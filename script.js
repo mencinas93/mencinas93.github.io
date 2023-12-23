c0bb3b4920d4ad59a5541b0302aa6b2ca0919e35
Michael Encinas <115640100+mencinas93@users.noreply.github.com>
2023-12-23 09:31:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,34 @@
 // script.js
 console.log('Hello from JavaScript!');
 
-// Add more JavaScript code here
+// Function to initialize the slideshow
+function initSlideshow() {
+    let slideIndex = 0;
+    showSlides();
+
+    // Function to show slides
+    function showSlides() {
+        let i;
+        const slides = document.getElementsByClassName('mySlides');
+
+        // Hide all slides
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = 'none';
+        }
+
+        // Increment index and reset if necessary
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+
+        // Display the current slide
+        slides[slideIndex - 1].style.display = 'block';
+
+        // Call the function again after a delay (e.g., 2000 milliseconds)
+        setTimeout(showSlides, 2000); // Adjust the delay as needed
+    }
+}
+
+// Call the initialization function
+initSlideshow();
